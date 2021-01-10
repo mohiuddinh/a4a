b7248896ba59b2b0bcb1d403932d54282226adcd
Hao <hran.wen@gmail.com>
2021-01-10 16:12:23 -0500

    home-background-icons

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,8 @@ import { Router } from "@reach/router";
 import NotFound from "./pages/NotFound.js";
 import Skeleton from "./pages/Skeleton.js";
 import Header from "./pages/Header.js";
+import Background from "./pages/Background.js";
+import Home from "./pages/Home.js";
 
 import "../css/utilities.css";
 import "../css/App.css";
@@ -59,6 +61,8 @@ class App extends Component {
           <NotFound default />
         </Router> */}
         <Header />
+        <Background />
+        <Home />
       </>
     );
   }
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "../../css/Home.css";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <input type="text" placeholder="ask away!" />
+      </div>
+    );
+  }
+}
+
+export default Home;
