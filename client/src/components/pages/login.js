bd277925e6881c29bfd063fd70a2d90447ade440
Hao <hran.wen@gmail.com>
2021-01-11 13:37:15 -0500

    layout + styling

--- client/src/components/pages/login.js
+++ client/src/components/pages/login.js
@@ -1,11 +1,37 @@
-import React from 'react'; 
+import React, { Component } from "react";
 
-function Login(){
-  return(
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  )
+import "../../css/Login.css";
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="login">
+        <div className="login__container">
+          <form action="" method="POST">
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              className="login__textInput"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              className="login__textInput"
+              required
+            />
+            <input type="submit" value="Login" className="login__btnInput btn" required />
+            <div className="login__otherSelection">
+              <h5>Forgot password?</h5>
+              <h5>Create an account.</h5>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Login; 
+export default Login;
--- client/src/components/pages/register.js
+++ client/src/components/pages/register.js
@@ -1,11 +1,63 @@
-import React from 'react'; 
+import React, { Component } from "react";
 
-function Register(){
-  return(
-    <div>
-      <h1>Register Page</h1>
-    </div>
-  )
+import "../../css/Register.css";
+
+class Register extends Component {
+  render() {
+    return (
+      <div className="register">
+        <div className="register__container">
+          <form action="" method="POST">
+            <div className="register__nameContainer">
+              <input
+                type="text"
+                name="firstname"
+                placeholder="first name"
+                className="register__textInput"
+                required
+              />
+              <input
+                type="text"
+                name="lastname"
+                placeholder="last name"
+                className="register__textInput"
+                required
+              />
+            </div>
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              className="register__textInput"
+              required
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="email"
+              className="register__textInput"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              className="register__textInput"
+              required
+            />
+            <input
+              type="password"
+              name="confirmpassword"
+              placeholder="confirm password"
+              className="register__textInput"
+              required
+            />
+            <input type="submit" value="Register" className="register__btnInput btn" required />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Register; 
+export default Register;
