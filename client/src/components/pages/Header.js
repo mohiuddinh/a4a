f48d57c9e4f3348b0ebea25e5946c97d2a0b6cc0
Hao <hran.wen@gmail.com>
2021-01-12 10:11:44 -0500

    btn hover transitions + fixed form overlap when resizing

--- client/src/components/pages/Header.js
+++ client/src/components/pages/Header.js
@@ -2,40 +2,45 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import "../../css/Header.css";
+
 const link_style = {
-  textDecoration: 'inherit',
-  color: 'inherit'
-}
+  textDecoration: "inherit",
+  color: "inherit",
+};
 
 class Header extends Component {
   render() {
     return (
       <div className="header">
         <div className="header__selection">
-          <h5> 
+          <h5>
             <Link to="/questions" style={link_style}>
               Questions
-            </Link> 
+            </Link>
           </h5>
-          <h5> 
+          <h5>
             <Link to="/post" style={link_style}>
               Post
-            </Link> 
+            </Link>
           </h5>
         </div>
         <div className="header__title">
-          <h1> 
+          <h1>
             <Link to="/" style={link_style}>
               MIT Ask
-            </Link> 
+            </Link>
           </h1>
         </div>
         <div className="header__user">
-          <button className='btn'> 
-            <Link to='/login' style={link_style}><span>Login</span></Link> 
-          </button> 
-          <button className="btn"> 
-            <Link to='/register' style={link_style}><span>Register</span></Link> 
+          <button className="btn">
+            <Link to="/login" style={link_style}>
+              <span>Login</span>
+            </Link>
+          </button>
+          <button className="btn">
+            <Link to="/register" style={link_style}>
+              <span>Register</span>
+            </Link>
           </button>
         </div>
       </div>
